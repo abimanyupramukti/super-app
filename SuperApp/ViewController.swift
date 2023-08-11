9d563f714b93af5db21d736185c53dd22b6d3e70
Abimanyu Pramukti <abimanyu.pramukti@stockbit.com>
2023-08-11 18:46:53 +0700

    initial commit

---
+++ SuperApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SuperApp
+//
+//  Created by Abimanyu Pramukti on 11/08/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
